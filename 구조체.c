6b707bce19e49e30eddb43b8cd04515e4539565f
EomJinHwan <167515443+EomJinHwan@users.noreply.github.com>
2025-09-23 20:42:52 +0900

    Add files via upload

---
+++ 구조체.c
@@ -0,0 +1,29 @@
+//구조체
+#include <stdio.h>
+struct Student {
+    char gender;
+    int age;
+};
+int main(){
+    struct Student s = {'F', 21};
+    s.gender = 'M';
+    printf("%c", s.gender);
+    printf("%d", s.age);
+    return 0;
+}
+
+//typedef
+#include <stdio.h>
+
+typedef struct Student{
+    char gender;
+    int age;
+} S;
+
+int main(){
+    S s = {'F', 21};
+    s.gender = 'M';
+    printf("%c", s.gender);
+    printf("%d", s.age);
+    return 0;
+}
